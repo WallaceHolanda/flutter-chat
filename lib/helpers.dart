30edff192de5a221f2fddabf325bc45d9b4ee972
WallaceDHolanda <wallace.holanda@popcode.com.br>
2023-02-25 21:51:52 -0300

    feat: cria helpers

---
+++ lib/helpers.dart
@@ -0,0 +1,15 @@
+import 'dart:math';
+
+class Helpers {
+  static final random = Random();
+
+  static String randomPictureUrl() {
+    final randomInt = random.nextInt(1000);
+    return 'https://picsum.photos/seed/$randomInt/300/300';
+  }
+
+  static DateTime randomDate() {
+    final currentDate = DateTime.now();
+    return currentDate.subtract(Duration(seconds: random.nextInt(200000)));
+  }
+}
