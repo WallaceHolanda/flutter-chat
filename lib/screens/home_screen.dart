ba0cd7eb9ad7b4725c20ed202fa281b0a8435dd8
WallaceDHolanda <wallace.holanda@popcode.com.br>
2023-02-25 21:52:52 -0300

    feat: adiciona app bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/helpers.dart';
 import 'package:flutter_chat/pages/calls_page.dart';
 import 'package:flutter_chat/pages/contacts_page.dart';
 import 'package:flutter_chat/pages/messages_page.dart';
 import 'package:flutter_chat/pages/notifications_page.dart';
-import 'package:flutter_chat/screens/widgets/bottom_navigation_bar.dart';
+import 'package:flutter_chat/screens/widgets/widgets.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,6 +15,14 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final ValueNotifier<int> pageIndex = ValueNotifier(0);
+  final ValueNotifier<String> pageTitle = ValueNotifier('Messages');
+
+  final pageTitles = const [
+    'Messages',
+    'Notifications',
+    'Calls',
+    'Contacts',
+  ];
 
   final pages = const [
     MessagesPage(),
@@ -22,9 +31,47 @@ class _HomeScreenState extends State<HomeScreen> {
     ContactsPage(),
   ];
 
+  void onNavigationItemSelected(int index) {
+    pageIndex.value = index;
+    pageTitle.value = pageTitles[index];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: ValueListenableBuilder(
+          valueListenable: pageTitle,
+          builder: (context, value, _) {
+            return Center(
+              child: Text(
+                value,
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            );
+          },
+        ),
+        leadingWidth: 54,
+        leading: Align(
+          alignment: Alignment.centerRight,
+          child: IconBackground(
+            icon: Icons.search,
+            onTap: () => debugPrint('Cliquei..'),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Avatar.small(url: Helpers.randomPictureUrl()),
+          )
+        ],
+      ),
       body: ValueListenableBuilder(
         valueListenable: pageIndex,
         builder: (context, int value, _) {
@@ -32,7 +79,7 @@ class _HomeScreenState extends State<HomeScreen> {
         },
       ),
       bottomNavigationBar: BottomNavigationBarWidget(
-        onItemSelected: (index) => pageIndex.value = index,
+        onItemSelected: onNavigationItemSelected,
       ),
     );
   }
