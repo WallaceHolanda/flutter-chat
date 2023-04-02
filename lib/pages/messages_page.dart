e9dfa3decfe860312970445b9f84c824bac1b7ef
Wallace Holanda <31262152+WallaceHolanda@users.noreply.github.com>
2023-04-02 00:49:14 +0000

    feat: adiciona stories na pagina de mensagens

--- lib/pages/messages_page.dart
+++ lib/pages/messages_page.dart
@@ -1,12 +1,110 @@
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/helpers.dart';
+import 'package:flutter_chat/models/models.dart';
+import 'package:flutter_chat/screens/widgets/widgets.dart';
+import 'package:flutter_chat/theme.dart';
 
 class MessagesPage extends StatelessWidget {
   const MessagesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('MessagesPage'),
+    return CustomScrollView(
+      slivers: [
+        const SliverToBoxAdapter(
+          child: _Stories(),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+              return const Text('Mensagens');
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Stories extends StatelessWidget {
+  const _Stories();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(8),
+      elevation: 0,
+      child: SizedBox(
+        height: 120,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 16, top: 8, bottom: 8),
+              child: Text(
+                'Stories',
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 15,
+                  color: AppColors.textFaded,
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) {
+                  final faker = Faker();
+                  return Padding(
+                    padding: const EdgeInsets.only(top: 6),
+                    child: SizedBox(
+                      width: 80,
+                      child: _StoryCard(
+                        storyData: StoryData(
+                          name: faker.person.name().split(" ")[0],
+                          url: Helpers.randomPictureUrl(),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _StoryCard extends StatelessWidget {
+  final StoryData storyData;
+
+  const _StoryCard({required this.storyData});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Avatar.medium(url: storyData.url),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 6),
+            child: Text(
+              storyData.name,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                fontSize: 11,
+                letterSpacing: 0.3,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
